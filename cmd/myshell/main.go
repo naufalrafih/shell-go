96bf44d28d416c9dac44d9224a02f2275cce9adf
naufal <naufalrafihh@gmail.com>
2024-09-30 10:07:46 +0700

    add code debugging

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,8 +10,8 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
+	// Debug
+	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
